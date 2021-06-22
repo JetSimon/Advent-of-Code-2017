d1c5a4bc62ef3914ff6a5425c7afb35940da84bc
Jet Simon <jetjdsimon@gmail.com>
2021-06-22 15:09:30 -0700

    Update day15.py

--- Day 15/day15.py
+++ Day 15/day15.py
@@ -15,8 +15,8 @@ def fillStack(n, factor, mul,name="",fill=5000000):
     stack = []
     while len(stack) < fill:
         n = generate(n, factor, 2147483647)
-        if a % mul:
-            stack.append(a)
+        if n % mul:
+            stack.append(n)
             print("stack",name,"at %", 100*(len(stack) / fill))
     return stack
 
@@ -26,26 +26,24 @@ b = 8921
 stacka = fillStack(a, 16807, 4,"A")
 stackb = fillStack(b, 48271, 8,"B")
 
-
-
-
-
+print("stack a has",len(stacka),"stack b has",len(stackb))
 
 count = 0
 pairs = 0
-while len(stacka) > 0 and len(stackb) > 0:
-    fa = stacka.pop(0)
-    fb = stackb.pop(0)
+
+print("starting judgement")
+
+for i in range(5000000):
+    fa = stacka[i]
+    fb = stackb[i]
     matching = judge(fa,fb)
 
     count = count + 1 if matching else count
     pairs += 1
 
     per = 100*(pairs/5000000)
+
     if(per % 1 == 0):
         print(per,"% complete")
-
-    if pairs == 5000000:
-        running = False
 
 print("FINAL COUNT:",count)
