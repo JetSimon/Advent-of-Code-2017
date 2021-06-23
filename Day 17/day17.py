62a51bb2f58fdb3b6a680f09b30dfa571c47dbf3
Jet Simon <jetjdsimon@gmail.com>
2021-06-22 19:13:40 -0700

    17 - got help w part 2

--- Day 17/day17.py
+++ Day 17/day17.py
@@ -1,11 +1,28 @@
-arr = [0]*2018
+arr = [0]
 pos = 0
-steps = 3
+steps = 394
 
-for n in range(1,2018):
+""" for n in range(1,50000000):
     pos += steps
-    arr.insert(pos+1,n)
-    pos = n
+    pos = pos % len(arr)
+    i = pos + 1
+    arr.insert(i,n)
+    pos = i
+    per = (n / 50000000) * 100
+    print( arr[arr.index(0) + 1] )
+#print(arr) """
 
-print(arr)
-print( arr[arr.index(2017) + 1] )
+steps = 394
+# Part 2
+curl = 1
+pos = 0
+out = 0
+for i in range(50000000):
+    to_ins = i+1
+    new = (pos + steps) % curl
+    new += 1
+    if new == 1:
+        out = to_ins
+    pos = new
+    curl += 1
+print(out)
